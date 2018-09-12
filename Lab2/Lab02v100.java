0c313ca3cf423812a7090b4484839512562695aa
avinab20 <avina.b20@student.parishepiscopal.org>
2018-09-12 14:46:09 -0500

    Added lab 2

---
+++ Lab2/Lab02v100.java
@@ -0,0 +1,31 @@
+/**
+ * Lab02v100.java  
+ *
+ * @author - Avina Bansal
+ * @author - B4
+ 
+ *
+ * @author - I received help from ...
+ *
+ */   
+public class Lab02v100
+
+{
+    public static void main(String[] args)
+    {
+    System.out.println("Lab 02, 80 Point Version");
+    System.out.println();
+    System.out.println();
+    System.out.print("Measure today's knowledge ");
+    System.out.println("by yesterday's confusion");
+    System.out.println();
+    System.out.println("Bewilderment + Exposure = Obvious");
+    System.out.println();
+    System.out.println("# ##### #####      ##### ##### # #   # ##### #####");
+    System.out.println("# #   # #   #      #   # #   # # ##  #   #   #    ");
+    System.out.println("# #   # #   #      ##### #   # # # # #   #   #####");
+    System.out.println("# #   # #   #      #     #   # # #  ##   #       #");
+    System.out.println("# ##### #####      #     ##### # #   #   #   #####");
+    
+}
+}
